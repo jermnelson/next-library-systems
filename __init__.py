330e85110120426d345e35cc0d3953151675670c
Jeremy Nelson <jermnelson@gmail.com>
2014-03-04 06:01:38 -0700

    added templates and working on UI

--- __init__.py
+++ __init__.py
@@ -13,13 +13,28 @@ import datetime
 import os
 import sys
 
+from collections import OrderedDict
 from flask import abort, Flask, g, jsonify, redirect, render_template, request
 
 app = Flask(__name__)
 
+slides = OrderedDict()
+slides['our-future-tracks-the-book'] = 'Our Future Tracks the book'
+slides['discovery-happens-elsewhere']= 'Discovery happens elsewhere...'
+slides['but-local-is-needed-more-than-ever'] = '...but local is needed more than ever'
+slides['beyond-mobile'] = 'Beyond Mobile'
+slides['heads-up-augmented-services'] = 'Heads up! Augmented Services'
+slides['back-to-the-future'] = 'Back to the Future?!'
+
+@app.route("/<slide>.html")
+def slide(slide):
+    return  render_template("{}.html".format(slide),
+                            slides=slides)
+
 @app.route("/")
 def default():
-    return render_template("index.html")
+    return render_template("index.html",
+                           slides=slides)
 
 def main(dev=False):
     host = '0.0.0.0'
